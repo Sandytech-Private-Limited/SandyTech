93a58e0b098b27aae5750f65406d8b033e81ad7c
websabre <sandeep.kothapalli1@gmail.com>
2025-03-16 17:22:40 +0530

    UI change for address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,11 +15,11 @@ const Contact = () => {
             style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
           ></iframe>
           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
+            <div className="lg:w-1 px-6">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
               <p className="mt-1 text-gray-700">Hyderabad, India</p>
             </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <div className="lg:w-1 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
               <a className="text-gray-500 leading-relaxed">sandeep.kothapalli1@gmail.com</a>
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
